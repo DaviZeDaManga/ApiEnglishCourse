27feee9adfea29530cca329ff7f8bff40472c3ab
Davi Matinho <129508589+DaviZeDaManga@users.noreply.github.com>
2024-07-16 03:53:40 -0300

    continuar

--- api/src/controller/professorController.js
+++ api/src/controller/professorController.js
@@ -3,41 +3,34 @@ const server = Router();
 
 import Joi from "Joi";
 import multer from "multer"; 
-import { inserirSala } from "../repository/professorRepository.js";
+import { dadosAvisosProfessor, dadosTransmissoesProfessor, dadosTrilhasProfessor, inserirAviso, inserirSala, inserirTransmissao, inserirTrilha } from "../repository/professorRepository.js";
 
 // Configuração do Multer
 const upload = multer({
     dest: 'uploads/images'
 });
 
+//inserir sala
 server.post('/professor/:idprofessor/novo/sala', upload.single('imagem'), async (req, resp) => {
     try {
-        const { idprofessor } = req.params;
+        const {idprofessor} = req.params;
         const idschema = Joi.number().integer().positive();
-        const { error: errorId } = idschema.validate(idprofessor);
-        if (errorId) {
-            return resp.status(400).send({ erro: 'O parâmetro "id" do professor é obrigatório.' });
-        }
+        const {error: errorId} = idschema.validate(idprofessor);
+        if (errorId) { return resp.status(400).send({ erro: 'O parâmetro "id" do professor é obrigatório.' }); }
 
         const schema = Joi.object({
             nome: Joi.string().required(),
             desc: Joi.string().required()
         });
-        const { error, value } = schema.validate(req.body);
-        if (error) {
-            return resp.status(400).send({ erro: 'Os parâmetros da resposta estão incorretos.', detalhes: error.details });
-        }
+        const {error, value} = schema.validate(req.body);
+        if (error) { return resp.status(400).send({ erro: 'Os parâmetros da resposta estão incorretos.', detalhes: error.details });}
 
         const imagemPath = req.file ? req.file.path : null;
-        if (!imagemPath) {
-            return resp.status(400).send({ erro: 'Imagem é obrigatória.' });
-        }
+        if (!imagemPath) { return resp.status(400).send({ erro: 'Imagem é obrigatória.' });}
 
-        const dados = { ...value, imagem: imagemPath };
+        const dados = {...value, imagem: imagemPath};
         const resposta = await inserirSala(idprofessor, dados);
-        if (resposta.affectedRows === 0) {
-            return resp.status(400).send({ erro: 'Nada foi adicionado.' });
-        }
+        if (resposta.affectedRows === 0) { return resp.status(400).send({ erro: 'Nada foi adicionado.' }); }
         return resp.send(resposta);
     } 
     catch (err) {
@@ -45,5 +38,161 @@ server.post('/professor/:idprofessor/novo/sala', upload.single('imagem'), async 
         resp.status(500).send({ erro: 'Erro interno no servidor', detalhes: err.message });
     }
 });
+
+
+
+//dados TRILHAS professor
+server.get("/professor/:idprofessor/dados/trilhas", async (req, resp)=> {
+    try {
+        const {idprofessor} = req.params;
+        const idschema = Joi.number().integer().positive().required()
+        const {error} = idschema.validate(idprofessor)
+        if (error) { return resp.status(400).send({ erro: 'O parâmetro "id" do usuário é obrigatório.'})}
+
+        const resposta = await dadosTrilhasProfessor(idprofessor)
+        if (!resposta) { return resp.status(400).send({ erro: 'Nada foi retornado.'})}
+        else if (resposta.length == 0) { return resp.status(400).send({ erro: 'Nenhuma trilha foi retornada.'})}
+        else { return resp.send(resposta)}
+    }
+    catch(err) {
+        console.error('Erro interno no servidor:', err);
+        resp.status(500).send({ erro: 'Erro interno no servidor' });
+    }
+})
+
+//insert trilha
+server.post('/professor/:idprofessor/novo/trilha', upload.single('imagem'), async (req, resp)=> {
+    try {
+        const {idprofessor} = req.params;
+        const idschema = Joi.number().integer().positive();
+        const {error: errorId} = idschema.validate(idprofessor);
+        if (errorId) { return resp.status(400).send({ erro: 'O parâmetro "id" do professor é obrigatório.' }); }
+
+        const schema = Joi.object({
+            nome: Joi.string().required(),
+            desc: Joi.string().required()
+        })
+        const {error, value} = schema.validate(req.body);
+        if (error) { return resp.status(400).send({ erro: 'Os parâmetros da resposta estão incorretos.', detalhes: error.details });}
+
+        const imagemPath = req.file ? req.file.path : null;
+        if (!imagemPath) { return resp.status(400).send({ erro: 'Imagem é obrigatória.' });}
+
+        const dados = {...value, imagem: imagemPath};
+        const resposta = await inserirTrilha(idprofessor, dados);
+        if (resposta.affectedRows === 0) { return resp.status(400).send({ erro: 'Nada foi adicionado.' }); }
+        return resp.send(resposta);
+    }
+    catch(err) {
+        console.error('Erro interno no servidor:', err);
+        resp.status(500).send({ erro: 'Erro interno no servidor', detalhes: err.message });
+    }
+})
+
+
+
+//dados AVISOS professor
+server.get("/professor/:idprofessor/dados/avisos", async (req, resp)=> {
+    try {
+        const {idprofessor} = req.params;
+        const idschema = Joi.number().integer().positive().required()
+        const {error} = idschema.validate(idprofessor)
+        if (error) { return resp.status(400).send({ erro: 'O parâmetro "id" do usuário é obrigatório.'})}
+
+        const resposta = await dadosAvisosProfessor(idprofessor)
+        if (!resposta) { return resp.status(400).send({ erro: 'Nada foi retornado.'})}
+        else if (resposta.length == 0) { return resp.status(400).send({ erro: 'Nenhum aviso foi retornado.'})}
+        else { return resp.send(resposta)}
+    }
+    catch(err) {
+        console.error('Erro interno no servidor:', err);
+        resp.status(500).send({ erro: 'Erro interno no servidor' });
+    }
+})
+
+//inserir aviso
+server.post('/professor/:idprofessor/novo/aviso', upload.single('imagem'), async (req, resp)=> {
+    try {
+        const {idprofessor} = req.params;
+        const idschema = Joi.number().integer().positive();
+        const {error: errorId} = idschema.validate(idprofessor);
+        if (errorId) { return resp.status(400).send({ erro: 'O parâmetro "id" do professor é obrigatório.' }); }
+
+        const schema = Joi.object({
+            nome: Joi.string().required(),
+            desc: Joi.string().required(),
+            video: Joi.string().required(),
+            comentarios: Joi.boolean().required(),
+            status: Joi.string().required()
+        })
+        const {error, value} = schema.validate(req.body);
+        if (error) { return resp.status(400).send({ erro: 'Os parâmetros da resposta estão incorretos.', detalhes: error.details });}
+
+        const imagemPath = req.file ? req.file.path : null;
+        if (!imagemPath) { return resp.status(400).send({ erro: 'Imagem é obrigatória.' });}
+
+        const dados = {...value, imagem: imagemPath};
+        const resposta = await inserirAviso(idprofessor, dados);
+        if (resposta.affectedRows === 0) { return resp.status(400).send({ erro: 'Nada foi adicionado.' }); }
+        return resp.send(resposta);
+    }
+    catch(err) {
+        console.error('Erro interno no servidor:', err);
+        resp.status(500).send({ erro: 'Erro interno no servidor', detalhes: err.message });
+    }
+})
+
+
+
+//dados TRANSMISSOES professor
+server.get("/professor/:idprofessor/dados/transmissoes", async (req, resp)=> {
+    try {
+        const {idprofessor} = req.params;
+        const idschema = Joi.number().integer().positive().required()
+        const {error} = idschema.validate(idprofessor)
+        if (error) { return resp.status(400).send({ erro: 'O parâmetro "id" do usuário é obrigatório.'})}
+
+        const resposta = await dadosTransmissoesProfessor(idprofessor)
+        if (!resposta) { return resp.status(400).send({ erro: 'Nada foi retornado.'})}
+        else if (resposta.length == 0) { return resp.status(400).send({ erro: 'Nenhuma transmissão foi retornada.'})}
+        else { return resp.send(resposta)}
+    }
+    catch(err) {
+        console.error('Erro interno no servidor:', err);
+        resp.status(500).send({ erro: 'Erro interno no servidor' });
+    }
+})
+
+//inserir transmissao
+server.post('/professor/:idprofessor/novo/transmissao', upload.single('imagem'), async (req, resp)=> {
+    try {
+        const {idprofessor} = req.params;
+        const idschema = Joi.number().integer().positive();
+        const {error: errorId} = idschema.validate(idprofessor);
+        if (errorId) { return resp.status(400).send({ erro: 'O parâmetro "id" do professor é obrigatório.' }); }
+
+        const schema = Joi.object({
+            nome: Joi.string().required(),
+            desc: Joi.string().required(),
+            video: Joi.string().required(),
+            comentarios: Joi.boolean().required(),
+            status: Joi.string().required()
+        })
+        const {error, value} = schema.validate(req.body);
+        if (error) { return resp.status(400).send({ erro: 'Os parâmetros da resposta estão incorretos.', detalhes: error.details });}
+
+        const imagemPath = req.file ? req.file.path : null;
+        if (!imagemPath) { return resp.status(400).send({ erro: 'Imagem é obrigatória.' });}
+
+        const dados = {...value, imagem: imagemPath};
+        const resposta = await inserirTransmissao(idprofessor, dados);
+        if (resposta.affectedRows === 0) { return resp.status(400).send({ erro: 'Nada foi adicionado.' }); }
+        return resp.send(resposta);
+    }
+    catch(err) {
+        console.error('Erro interno no servidor:', err);
+        resp.status(500).send({ erro: 'Erro interno no servidor', detalhes: err.message });
+    }
+})
 
 export default server;
--- api/src/repository/professorRepository.js
+++ api/src/repository/professorRepository.js
@@ -6,6 +6,132 @@ export async function inserirSala(idprofessor, dados) {
     INSERT INTO tb_salas (id_professor, nm_nome, ds_descricao, img_imagem, dt_criado)
     VALUES (?, ?, ?, ?, curdate());`
 
-    const [resposta] = await conx.query(comando, [idprofessor, dados.nome, dados.desc, dados.imagem])
-    return resposta
+    try {
+        const [resposta] = await conx.query(comando, [idprofessor, dados.nome, dados.desc, dados.imagem])
+        return resposta
+    }
+    catch (error) {
+        console.error('Erro ao inserir uma nova sala:', error);
+        throw error; 
+    }
+}
+
+
+
+//dados trilhas professor
+export async function dadosTrilhasProfessor(idprofessor) {
+    const comando = `
+    SELECT 
+    tt.id_trilha AS id,
+    tt.id_professor AS professor, 
+    tt.nm_nome AS nome, 
+    tt.ds_descricao AS descricao, 
+    tt.img_imagem AS imagem, 
+    tt.dt_criado AS criado
+    FROM tb_trilhas tt
+    WHERE tt.id_professor = ?`
+
+    try {
+        const [linhas] = await conx.query(comando, [idprofessor]);
+        return linhas;
+    } catch (error) {
+        console.error('Erro ao executar consulta dos dados das trilhas:', error);
+        throw error; 
+    }
+}
+
+//insert trilha
+export async function inserirTrilha(idprofessor, dados) {
+    const comando = `
+    INSERT INTO tb_trilhas (id_professor, nm_nome, ds_descricao, img_imagem, dt_criado)
+    VALUES (?, ?, ?, ?, CURDATE())`
+
+    try {
+        const [resposta] = await conx.query(comando, [idprofessor, dados.nome, dados.desc, dados.imagem])
+        return resposta
+    }
+    catch (error) {
+        console.error('Erro ao inserir uma nova trilha:', error);
+        throw error; 
+    }
+}
+
+
+
+//dados avisos professor
+export async function dadosAvisosProfessor(idprofessor) {
+    const comando = `
+    SELECT 
+    ta.id_aviso AS id,
+    ta.id_professor AS professor, 
+    ta.nm_nome AS nome, 
+    ta.ds_descricao AS descricao, 
+    ta.img_imagem AS imagem, 
+    ta.dt_criado AS criado
+    FROM tb_avisos ta
+    WHERE ta.id_professor = ?`
+
+    try {
+        const [linhas] = await conx.query(comando, [idprofessor]);
+        return linhas;
+    } catch (error) {
+        console.error('Erro ao executar consulta dos dados dos avisos:', error);
+        throw error; 
+    }
+}
+
+//insert aviso
+export async function inserirAviso(idprofessor, dados) {
+    const comando = `
+    INSERT INTO tb_avisos (id_professor, nm_nome, ds_descricao, img_imagem, url_video, bt_comentarios, ds_status, dt_criado)
+    VALUES (?, ?, ?, ?, ?, ?, CURDATE())`
+
+    try {
+        const [resposta] = await conx.query(comando, [idprofessor, dados.nome, dados.desc, dados.imagem, dados.video, dados.comentarios, dados.status])
+        return resposta
+    }
+    catch (error) {
+        console.error('Erro ao inserir um novo aviso:', error);
+        throw error; 
+    }
+}
+
+
+
+//dados transmissoes professor
+export async function dadosTransmissoesProfessor(idprofessor) {
+    const comando = `
+    SELECT 
+    tt.id_transmissao AS id,
+    tt.id_professor AS professor, 
+    tt.nm_nome AS nome, 
+    tt.ds_descricao AS descricao, 
+    tt.img_imagem AS imagem, 
+    tt.dt_criado AS criado
+    FROM tb_transmissoes tt
+    WHERE tt.id_professor = ?`
+
+    try {
+        const [linhas] = await conx.query(comando, [idprofessor]);
+        return linhas;
+    } catch (error) {
+        console.error('Erro ao executar consulta dos dados das transmissões:', error);
+        throw error; 
+    }
+}
+
+//insert transmissao
+export async function inserirTransmissao(idprofessor, dados) {
+    const comando = `
+    INSERT INTO tb_transmissoes (id_professor, nm_nome, ds_descricao, img_imagem, url_video, bt_comentarios, ds_status, dt_criado)
+    VALUES (?, ?, ?, ?, ?, ?, CURDATE())`
+
+    try {
+        const [resposta] = await conx.query(comando, [idprofessor, dados.nome, dados.desc, dados.imagem, dados.video, dados.comentarios, dados.status])
+        return resposta
+    }
+    catch (error) {
+        console.error('Erro ao inserir uma nova transmissão:', error);
+        throw error; 
+    }
 }
